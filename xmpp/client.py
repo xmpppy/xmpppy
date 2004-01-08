af745a2ea2f5d240be56cd08d13ad776084219b6
Alexey Nezhdanov <snakeru@gmail.com>
2004-01-08 18:08:23 +0000

    Bugfixes: replaced "m" with "self" in many cases in Client code.

--- xmpp/client.py
+++ xmpp/client.py
@@ -25,6 +25,7 @@ Debug.Debug.colors['socket']=debug.color_dark_gray
 Debug.Debug.colors['CONNECTproxy']=debug.color_dark_gray
 Debug.Debug.colors['nodebuilder']=debug.color_brown
 Debug.Debug.colors['client']=debug.color_cyan
+Debug.Debug.colors['component']=debug.color_cyan
 Debug.Debug.colors['dispatcher']=debug.color_green
 Debug.Debug.colors['auth']=debug.color_yellow
 Debug.Debug.colors['roster']=debug.color_magenta
@@ -41,6 +42,7 @@ Debug.Debug.colors['sent']=debug.color_blue
 Debug.Debug.colors['got']=debug.color_bright_cyan
 
 DBG_CLIENT='client'
+DBG_COMPONENT='client'
 class Client:
     def __init__(self,server,debug=['socket']):#['always', 'nodebuilder']):
         self.disconnect_handlers=[]
@@ -51,6 +53,7 @@ class Client:
         self.debug_flags=Debug.debug_flags
         self.debug_flags.append(DBG_CLIENT)
         self._owner=self
+        self._registered_name=None
 
     def RegisterDisconnectHandler(self,handler):
         self.disconnect_handlers.append(handler)
@@ -67,23 +70,23 @@ class Client:
     def send_header(self):
         self.send("<?xml version='1.0'?><stream:stream version='1.0' xmlns:stream='http://etherx.jabber.org/streams' to='%s' xmlns='%s'>"%(self.Server,self.Namespace))
 
-    def connect(self,proxy=None):
-        if proxy: transports.HTTPPROXYsocket(proxy).PlugIn(self)
-        else: transports.TCPsocket().PlugIn(self)
-        dispatcher.Dispatcher().PlugIn(m)
+    def connect(self,server=None,proxy=None):
+        if proxy: transports.HTTPPROXYsocket(proxy,server).PlugIn(self)
+        else: transports.TCPsocket(server).PlugIn(self)
+        dispatcher.Dispatcher().PlugIn(self)
         self.send_header()
         transports.TLS().PlugIn(self)
         while self.Process() and not self.Dispatcher.Stream._document_attrs: pass
         if self.Dispatcher.Stream._document_attrs.has_key('version') and self.Dispatcher.Stream._document_attrs['version']=='1.0':
             while self.Process() and not self.TLS.starttls: pass
-        if self.TLS.starttls<>'proceed': self.disconnected()
+            if self.TLS.starttls<>'proceed': self.disconnected()
 
     def auth(self,user,password,resource):
         auth.SASL(user,password).PlugIn(self)
         while self.Process() and not self.Dispatcher.Stream._document_attrs: pass
         if self.Dispatcher.Stream._document_attrs.has_key('version') and self.Dispatcher.Stream._document_attrs['version']=='1.0':
             while self.Process() and not self.SASL.startsasl: pass
-        if self.SASL.startsasl<>'success': auth.NonSASL(user,password,resource).PlugIn(m)
+        if self.SASL.startsasl<>'success': auth.NonSASL(user,password,resource).PlugIn(self)
         else:
             auth.Bind().PlugIn(self)
             self.Bind.Bind(resource)
@@ -92,25 +95,38 @@ class Client:
         self.sendPresence(requestRoster=requestRoster)
 
     def sendPresence(self,jid=None,type=None,requestRoster=0):
-        if requestRoster: roster.Roster().PlugIn(m)
+        if requestRoster: roster.Roster().PlugIn(self)
         self.send(dispatcher.protocol.Presence(to=jid, type=type))
 
-test_client=1
-if test_client:
-    proxy={}
-    proxy['host']='localhost'
-    proxy['port']=8080
-    proxy['user']='3128'
-    proxy['password']='3128'
-    m=Client('woody8.penza-gsm.ru')
-    m.connect()
-    #raise
-    m.auth('test','test','test')
-    m.sendInitPresence()
-    if 1:
-        m.Process(5)
-        print m.Roster._data
-    print discoverItems(m,'woody8.penza-gsm.ru')
-    print discoverInfo(m,'woody8.penza-gsm.ru')
-print getPrivacyLists(m)
-print getPrivacyList(m,'a')
+class Component:
+    def __init__(self,server,port,debug=['always', 'nodebuilder']):
+        self.disconnect_handlers=[]
+        self.Namespace='jabber:component:accept'
+        self.Server=server
+        self.Port=port
+        self._DEBUG=Debug.Debug(debug)
+        self.DEBUG=self._DEBUG.Show
+        self.debug_flags=Debug.debug_flags
+        self.debug_flags.append(DBG_COMPONENT)
+        self._owner=self
+        self._registered_name=None
+
+    def RegisterDisconnectHandler(self,handler):
+        self.disconnect_handlers.append(handler)
+
+    def DeregisterDisconnectHandler(self,handler):
+        self.disconnect_handlers.remove(handler)
+
+    def send_header(self):
+        self.send("<?xml version='1.0'?><stream:stream version='1.0' xmlns:stream='http://etherx.jabber.org/streams' to='%s' xmlns='%s'>"%(self.Server,self.Namespace))
+
+    def connect(self,server=None,proxy=None):
+        if not server: server=(self.Server,self.Port)
+        if proxy: transports.HTTPPROXYsocket(proxy,server).PlugIn(self)
+        else: transports.TCPsocket(server).PlugIn(self)
+        dispatcher.Dispatcher().PlugIn(self)
+        self.send_header()
+        while self.Dispatcher.Stream._document_attrs is None: self.Process(1)
+
+    def auth(self,name,password):
+        auth.NonSASL(name,password,'').PlugIn(self)
