abe1647ba1a82eebbd00086b7f05757fbc206668
Alexey Nezhdanov <snakeru@gmail.com>
2004-04-18 06:24:03 +0000

    XMPP-Core stanza and stream level errors support added.

--- xmpp/protocol.py
+++ xmpp/protocol.py
@@ -17,8 +17,65 @@
 from simplexml import Node
 import time
 
-NS_DELAY='jabber:x:delay'
-NS_DATA ='jabber:x:data'
+NS_DELAY   = 'jabber:x:delay'
+NS_DATA    = 'jabber:x:data'
+NS_STANZAS = 'urn:ietf:params:xml:ns:xmpp-stanzas'
+
+xmpp_stream_error_conditions="""
+bad-format --  --  -- The entity has sent XML that cannot be processed.
+bad-namespace-prefix --  --  -- The entity has sent a namespace prefix that is unsupported, or has sent no namespace prefix on an element that requires such a prefix.
+conflict --  --  -- The server is closing the active stream for this entity because a new stream has been initiated that conflicts with the existing stream.
+connection-timeout --  --  -- The entity has not generated any traffic over the stream for some period of time.
+host-gone --  --  -- The value of the 'to' attribute provided by the initiating entity in the stream header corresponds to a hostname that is no longer hosted by the server.
+host-unknown --  --  -- The value of the 'to' attribute provided by the initiating entity in the stream header does not correspond to a hostname that is hosted by the server.
+improper-addressing --  --  -- A stanza sent between two servers lacks a 'to' or 'from' attribute (or the attribute has no value).
+internal-server-error --  --  -- The server has experienced a misconfiguration or an otherwise-undefined internal error that prevents it from servicing the stream.
+invalid-from -- cancel --  -- The JID or hostname provided in a 'from' address does not match an authorized JID or validated domain negotiated between servers via SASL or dialback, or between a client and a server via authentication and resource authorization.
+invalid-id --  --  -- The stream ID or dialback ID is invalid or does not match an ID previously provided.
+invalid-namespace --  --  -- The streams namespace name is something other than "http://etherx.jabber.org/streams" or the dialback namespace name is something other than "jabber:server:dialback".
+invalid-xml --  --  -- The entity has sent invalid XML over the stream to a server that performs validation.
+not-authorized --  --  -- The entity has attempted to send data before the stream has been authenticated, or otherwise is not authorized to perform an action related to stream negotiation.
+policy-violation --  --  -- The entity has violated some local service policy.
+remote-connection-failed --  --  -- The server is unable to properly connect to a remote resource that is required for authentication or authorization.
+resource-constraint --  --  -- The server lacks the system resources necessary to service the stream.
+restricted-xml --  --  -- The entity has attempted to send restricted XML features such as a comment, processing instruction, DTD, entity reference, or unescaped character.
+see-other-host --  --  -- The server will not provide service to the initiating entity but is redirecting traffic to another host.
+system-shutdown --  --  -- The server is being shut down and all active streams are being closed.
+undefined-condition --  --  -- The error condition is not one of those defined by the other conditions in this list.
+unsupported-encoding --  --  -- The initiating entity has encoded the stream in an encoding that is not supported by the server.
+unsupported-stanza-type --  --  -- The initiating entity has sent a first-level child of the stream that is not supported by the server.
+unsupported-version --  --  -- The value of the 'version' attribute provided by the initiating entity in the stream header specifies a version of XMPP that is not supported by the server.
+xml-not-well-formed --  --  -- The initiating entity has sent XML that is not well-formed."""
+xmpp_stanza_error_conditions="""
+bad-request -- 400 -- modify -- The sender has sent XML that is malformed or that cannot be processed.
+conflict -- 409 -- cancel -- Access cannot be granted because an existing resource or session exists with the same name or address.
+feature-not-implemented -- 501 -- cancel -- The feature requested is not implemented by the recipient or server and therefore cannot be processed.
+forbidden -- 403 -- auth -- The requesting entity does not possess the required permissions to perform the action.
+gone -- 302 -- modify -- The recipient or server can no longer be contacted at this address.
+internal-server-error -- 500 -- wait -- The server could not process the stanza because of a misconfiguration or an otherwise-undefined internal server error.
+item-not-found -- 404 -- cancel -- The addressed JID or item requested cannot be found.
+jid-malformed -- 400 -- modify -- The value of the 'to' attribute in the sender's stanza does not adhere to the syntax defined in Addressing Scheme.
+not-acceptable -- 406 -- cancel -- The recipient or server understands the request but is refusing to process it because it does not meet criteria defined by the recipient or server.
+not-allowed -- 405 -- cancel -- The recipient or server does not allow any entity to perform the action.
+payment-required -- 402 -- auth -- The requesting entity is not authorized to access the requested service because payment is required.
+recipient-unavailable -- 404 -- wait -- The intended recipient is temporarily unavailable.
+redirect -- 302 -- modify -- The recipient or server is redirecting requests for this information to another entity.
+registration-required -- 407 -- auth -- The requesting entity is not authorized to access the requested service because registration is required.
+remote-server-not-found -- 404 -- cancel -- A remote server or service specified as part or all of the JID of the intended recipient does not exist.
+remote-server-timeout -- 504 -- wait -- A remote server or service specified as part or all of the JID of the intended recipient could not be contacted within a reasonable amount of time.
+resource-constraint -- 500 -- wait -- The server or recipient lacks the system resources necessary to service the request.
+service-unavailable -- 503 -- cancel -- The server or recipient does not currently provide the requested service.
+subscription-required -- 407 -- auth -- The requesting entity is not authorized to access the requested service because a subscription is required.
+undefined-condition -- 500 --  -- 
+unexpected-request -- 400 -- wait -- The recipient or server understood the request but was not expecting it at this time (e.g., the request was out of order)."""
+
+ERRORS={}
+for err in (xmpp_stream_error_conditions+xmpp_stanza_error_conditions)[1:].split('\n'):
+    cond,code,typ,text=err.split(' -- ')
+    name='ERR_'+cond.upper().replace('-','_')
+    locals()[name]=cond
+    ERRORS[cond]=[code,typ,text]
+del err,cond,code,typ,text
 
 def isResultNode(node): return node and node.getType()=='result'
 def isErrorNode(node): return node and node.getType()=='error'
@@ -77,11 +134,16 @@ class Protocol(Node):
     def setFrom(self,val): self.setAttr('from', val)
     def setType(self,val): self.setAttr('type', val)
     def setID(self,val): self.setAttr('id', val)
-    def getError(self): return self.getTagData('error')
+    def getError(self):
+        for tag in self.getTag('error').getChildren():
+            if tag.getName()<>'text': return tag.getName()
+        return self.getTagData('error')
     def getErrorCode(self): return self.getTagAttr('error','code')
-    def setError(self,code,comment):
-        self.setTagData('error',comment)
-        self.setTagAttr('error','code',code)
+    def setError(self,code,comment=''):
+        if type(code) in [type(''),type(u'')]: error=Error(ERR_FEATURE_NOT_IMPLEMENTED,code=code,text=comment)
+        else: error=code
+        self.setType('error')
+        self.addChild(node=error)
     def setTimestamp(self,val=None):
         if not val: val=time.strftime('%Y%m%dT%H:%M:%S', time.gmtime())
         self.timestamp=val
@@ -152,3 +214,22 @@ class DataForm(Node):
     def setField(self,name, val): self.setTag('field',attrs={'var':name}).setTagData('value',val)
     def setFromDict(self,dict):
         for i in dict.keys(): self.setField(i,dict[i])
+
+class Error(Node):
+    def __init__(self,name,**argv):
+        """ Mandatory parameter: name
+            Optional parameters: code, type, text, baze_stanza.
+            baze_stanza is a node that will got error tag incorporated"""
+        if ERRORS.has_key(name): code,type,text=ERRORS[name]
+        else: code,type,text='','cancel',''
+        if argv.has_key('type'): type=argv['type']
+        if argv.has_key('code'): code=argv['code']
+        if argv.has_key('text'): text=argv['text']
+        Node.__init__(self,'error',{'type':type},[Node(NS_STANZAS+' '+name),Node(NS_STANZAS+' text',{},[text])])
+        if code: self.setAttr('code',code)
+        if argv.has_key('base_stanza'):
+            base=argv['base_stanza']
+            frm,to=base.getAttr('from'),base.getAttr('to')
+            if frm: base.setTo(frm)
+            if to: base.setFrom(to)
+            base.setError(self)
