4c837ec9a2c95ee1d7e138b502fa138722161af6
Alexey Nezhdanov <snakeru@gmail.com>
2004-04-25 05:12:01 +0000

    Date extended in license text.

--- xmpp/features.py
+++ xmpp/features.py
@@ -1,6 +1,6 @@
 ##   features.py 
 ##
-##   Copyright (C) 2003 Alexey "Snake" Nezhdanov
+##   Copyright (C) 2003-2004 Alexey "Snake" Nezhdanov
 ##
 ##   This program is free software; you can redistribute it and/or modify
 ##   it under the terms of the GNU General Public License as published by
