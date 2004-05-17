74a4c80ccbf40d3f6fb9fd72825c3d5e37b25219
Alexey Nezhdanov <snakeru@gmail.com>
2004-05-17 09:32:15 +0000

    Added NS_XXX importing into module's namespace.

--- xmpp/__init__.py
+++ xmpp/__init__.py
@@ -1,4 +1,9 @@
 # $Id$
 
-import simplexml,protocol,debug,auth,transports,roster,dispatcher,features
+for m in ['simplexml','protocol','debug','auth','transports','roster','dispatcher','features']:
+    exec 'import '+m
+    md=locals()[m].__dict__
+    for var in md.keys():
+        if var[:3]=='NS_': locals()[var]=md[var]
+
 from client import *
