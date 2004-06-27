0668405a79d4224ae40afc8efb82284765d81c87
Alexey Nezhdanov <snakeru@gmail.com>
2004-06-27 18:24:31 +0000

    Browser module tested, fixed and included into library structure.

--- xmpp/__init__.py
+++ xmpp/__init__.py
@@ -1,10 +1,5 @@
 # $Id$
 
-for m in ['simplexml','protocol','debug','auth','transports','roster','dispatcher','features']:
-    exec 'import '+m
-    md=locals()[m].__dict__
-    for var in md.keys():
-        if var[:3]=='NS_': locals()[var]=md[var]
-
+import simplexml,protocol,debug,auth,transports,roster,dispatcher,features,browser
 from client import *
 from protocol import *
--- xmpp/browser.py
+++ xmpp/browser.py
@@ -33,42 +33,22 @@ class Browser(PlugIn):
         self._owner.UnregisterHandler('iq',self._DiscoveryHandler,ns=NS_DISCO_ITEMS)
 
     def _traversePath(self,node,set=0):
-        "Returns dictionary and key or None,None"
-        "/a/b/c - node"
-        "/a/b/  - branch"
-        """ '' - handler for this branch
-            1  - handler for this node
-        """
-        {'a':{1:handlerA,'':HandlerAdef},None:{1:handlerRoot}}
-        "Set returns '' or None as the key "
-        "get returns '' or None as the key or None as the dict"
+        """Returns dictionary and key or None,None
+        None - root node (w/o "node" attribute)
+        /a/b/c - node
+        /a/b/  - branch
+        Set returns '' or None as the key
+        get returns '' or None as the key or None as the dict"""
 
-        """
-            1. set None   (root)
-            2. set a      (node)
-            3. set a/     (default)
-            4. set a//    (illegal - case 3)
-            5. set /      ([il]legal - like 3)
-
-            6. get None   (root)
-            7. get None   (non-existent)
-            8. get a      (node)
-            9. get a/b    (default)
-           10. get c      (non-existent)
-           11. get /      (illegal)
-           12. get a/     (illegal)
-           13. get a//    (illegal)
-                """
-        {None:{1:handlerRoot},'a':{1:handlerA, '':handlerAdef}}
         cur=self._handlers
-        if not node: node=[None]
+        if node is None: node=[None]
         else: node=node.split('/')
         for i in node:
-            if i<>'' and cur.has_key[i]: cur=cur[i]     # CYCLE: 1e,2e,3e,6,8,9,12,13
-            elif set and i<>'': cur[i]={}; cur=cur[i]   # CYCLE: 1n,2n,3n
-            elif set or cur.has_key(''): return cur,''  # 3e,3n,7d,9,11,12,13
-            else: return None,None                      # 7n,10
-        if cur.has_key(1) or set: return cur,1          # 1e,1n,2e,2n,6,8
+            if i<>'' and cur.has_key(i): cur=cur[i]
+            elif set and i<>'': cur[i]={dict:cur,str:i}; cur=cur[i]
+            elif set or cur.has_key(''): return cur,''
+            else: return None,None
+        if cur.has_key(1) or set: return cur,1
         raise "Corrupted data"
 
     def setDiscoHandler(self,Handler,node=''):
@@ -83,27 +63,26 @@ class Browser(PlugIn):
         node,key=self._traversePath(node)
         if node:
             handler=node[key]
-            del node[key]
+            del node[dict][node[str]]
             return handler
 
     def _DiscoveryHandler(self,conn,request):
-        node,key=self._traversePath(request.getQuerynode())
-        if not node or not node[key]:
-            return conn.send(Error(request,ERR_ITEM_NOT_FOUND))
+        handler=self.getDiscoHandler(request.getQuerynode())
+        if not handler: return conn.send(Error(request,ERR_ITEM_NOT_FOUND))
         rep=request.buildReply('result')
         q=rep.getTag('query')
         if request.getQueryNS()==NS_DISCO_ITEMS:
-            list=node[key]['items']
             # handler must return list: [{jid,action,node,name}]
-            if type(list)<>type([]): list=list(conn,request,'items')
-            for item in list: q.addChild('item',item)
+            if type(handler)==dict: lst=handler['items']
+            else: lst=handler(conn,request,'items')
+            for item in lst: q.addChild('item',item)
         elif request.getQueryNS()==NS_DISCO_INFO:
-            dict=node[key]['info']
+            if type(handler)==dict: dt=handler['info']
+            else: lst=handler(conn,request,'info')
             # handler must return dictionary:
             # {'ids':[{},{},{},{}], 'features':[fe,at,ur,es], 'xdata':DataForm}
-            if type(dict)<>type({}): dict=dict(conn,request,'info')
-            for id in dict['ids']: q.addChild('identity',id)
-            for feature in dict['features']: q.addChild('feature',{'var':feature})
-            if dict.has_key('xdata'): q.addChild(node=dict['xdata'])
+            for id in dt['ids']: q.addChild('identity',id)
+            for feature in dt['features']: q.addChild('feature',{'var':feature})
+            if dt.has_key('xdata'): q.addChild(node=dt['xdata'])
         conn.send(rep)
         raise NodeProcessed
