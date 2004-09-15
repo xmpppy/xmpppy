ea247ce2a51c7c42424ee8c0544391ff694b830d
Alexey Nezhdanov <snakeru@gmail.com>
2004-09-15 18:35:06 +0000

    Bugfix: more delicate namespaces processing. Slow (again) but sure.

--- xmpp/simplexml.py
+++ xmpp/simplexml.py
@@ -156,9 +156,10 @@ class NodeBuilder:
     def __init__(self,data=None,initial_node=None):
         self.DEBUG(DBG_NODEBUILDER, "Preparing to handle incoming XML stream.", 'start')
         self._parser = xml.parsers.expat.ParserCreate(namespace_separator=' ')
-        self._parser.StartElementHandler  = self.starttag
-        self._parser.EndElementHandler    = self.endtag
-        self._parser.CharacterDataHandler = self.handle_data
+        self._parser.StartElementHandler       = self.starttag
+        self._parser.EndElementHandler         = self.endtag
+        self._parser.CharacterDataHandler      = self.handle_data
+        self._parser.StartNamespaceDeclHandler = self.handle_namespace_start
         self.Parse = self._parser.Parse
 
         self.__depth = 0
@@ -166,15 +167,21 @@ class NodeBuilder:
         self._document_attrs = None
         self._mini_dom=initial_node
         self.last_is_data = 1
+        self._ptr=None
+        self.namespaces={"http://www.w3.org/XML/1998/namespace":'xml:'}
+        self.xmlns="http://www.w3.org/XML/1998/namespace"
 
         if data: self._parser.Parse(data,1)
 
     def starttag(self, tag, attrs):
         """XML Parser callback"""
-        for attr in attrs.keys():       # FIXME: Crude hack. And it also slows down the whole library considerably.
-            if attr[:37]=="http://www.w3.org/XML/1998/namespace ":
-                attrs['xml:'+attr[37:]]=attrs[attr]
-                del attrs[attr]
+        attlist=attrs.keys()       #
+        for attr in attlist:       # FIXME: Crude hack. And it also slows down the whole library considerably.
+            sp=attr.rfind(" ")     #
+            if sp==-1: continue    #
+            ns=attr[:sp]           #
+            attrs[self.namespaces[ns]+attr[sp+1:]]=attrs[attr]
+            del attrs[attr]        #
         self.__depth += 1
         self.DEBUG(DBG_NODEBUILDER, "DEPTH -> %i , tag -> %s, attrs -> %s" % (self.__depth, tag, `attrs`), 'down')
         if self.__depth == self._dispatch_depth:
@@ -186,6 +193,8 @@ class NodeBuilder:
             self._ptr = self._ptr.kids[-1]
         if self.__depth == 1:
             self._document_attrs = attrs
+            ns, name = (['']+tag.split())[-2:]
+            self.stream_header_received(ns, name, attrs)
         if not self.last_is_data and self._ptr.parent: self._ptr.parent.data.append('')
         self.last_is_data = 0
 
@@ -200,18 +209,25 @@ class NodeBuilder:
             self.DEBUG(DBG_NODEBUILDER, "Got higher than dispatch level. Stream terminated?", 'stop')
         self.__depth -= 1
         self.last_is_data = 0
+        if self.__depth == 0: self.stream_footer_received()
 
     def handle_data(self, data):
         """XML Parser callback"""
         self.DEBUG(DBG_NODEBUILDER, data, 'data')
+        if not self._ptr: return
         if self.last_is_data:
             self._ptr.data[-1] += data
         else:
             self._ptr.data.append(data)
             self.last_is_data = 1
 
+    def handle_namespace_start(self, prefix, uri):
+        if prefix: self.namespaces[uri]=prefix+':'
+        else: self.xmlns=uri
     def DEBUG(self, level, text, comment=None): pass
     def getDom(self): return self._mini_dom
     def dispatch(self,stanza): pass
+    def stream_header_received(self,ns,tag,attrs): pass
+    def stream_footer_received(self): pass
 
 def XML2Node(xml): return NodeBuilder(xml).getDom()
