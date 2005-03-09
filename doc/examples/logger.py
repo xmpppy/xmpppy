9abc7e76f36aa8fc9b01c2e87ab612389c50d4e9
Alexey Nezhdanov <snakeru@gmail.com>
2005-03-09 14:18:42 +0000

    Added presences tracking

--- doc/examples/logger.py
+++ doc/examples/logger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: koi8-r -*-
 from xmpp import *
 import time,os
 
@@ -11,7 +12,17 @@ PROXY={}
 #PROXY={'host':'192.168.0.1','port':3128,'username':'luchs','password':'secret'}
 #######################################
 
-def LOG(fold,day,tm,nick,text):
+def LOG(stanza,nick,text):
+    ts=stanza.getTimestamp()
+    if not ts:
+        ts=stanza.setTimestamp()
+        ts=stanza.getTimestamp()
+    tp=time.mktime(time.strptime(ts,'%Y%m%dT%H:%M:%S %Z'))+3600*3
+    if time.localtime()[-1]: tp+=3600
+    tp=time.localtime(tp)
+    fold=stanza.getFrom().getStripped().replace('@','%')+'_'+time.strftime("%Y.%m",tp)
+    day=time.strftime("%d",tp)
+    tm=time.strftime("%H:%M:%S",tp)
     try: os.mkdir(LOGDIR+fold)
     except: pass
     fName='%s%s/%s.%s.html'%(LOGDIR,fold,fold,day)
@@ -26,30 +37,35 @@ def LOG(fold,day,tm,nick,text):
     <body>
 <table border="1"><tr><th>time</th><th>who</th><th>text</th></tr>
 """%(CONF[0],fold,day))
+    text='<pre>%s</pre>'%text
     open(fName,'a').write((u"<tr><td>%s</td><td>%s</td><td>%s</td></tr>\n"%(tm,nick,text)).encode('utf-8'))
-    print u"<tr><td>%s</td><td>%s</td><td>%s</td></tr>\n"%(tm,nick,text)
+    print (u"<tr><td>%s</td><td>%s</td><td>%s</td></tr>\n"%(tm,nick,text)).encode('koi8-r','replace')
 #    print time.localtime(tp),nick,text
 
 def messageCB(sess,mess):
     nick=mess.getFrom().getResource()
     text=mess.getBody()
-    ts=mess.getTimestamp()
-    if not ts:
-        ts=mess.setTimestamp()
-        ts=mess.getTimestamp()
-    tp=time.mktime(time.strptime(ts,'%Y%m%dT%H:%M:%S %Z'))+3600*3
-    if time.localtime()[-1]: tp+=3600
-    tp=time.localtime(tp)
-    fold=time.strftime("%Y.%m",tp)
-    day=time.strftime("%d",tp)
-    tm=time.strftime("%H:%M:%S",tp)
-    line="<tr><td>%s</td><td>%s<td>"%(tm,text)
-    LOG(fold,day,tm,nick,text)
+    LOG(mess,nick,text)
+
+roster=[]
+def presenceCB(sess,pres):
+    nick=pres.getFrom().getResource()
+    text=''
+    if pres.getType()=='unavailable':
+        if nick in roster:
+            text=nick+unicode(' покинул конференцию','koi8-r')
+            roster.remove(nick)
+    else:
+        if nick not in roster:
+            text=nick+unicode(' пришёл в конференцию','koi8-r')
+            roster.append(nick)
+    if text: LOG(pres,nick,text)
 
 if 1:
     cl=Client(JID(BOT[0]).getDomain(),debug=[])
     cl.connect(proxy=PROXY)
     cl.RegisterHandler('message',messageCB)
+    cl.RegisterHandler('presence',presenceCB)
     cl.auth(JID(BOT[0]).getNode(),BOT[1])
     p=Presence(to='%s/logger'%CONF[0])
     p.setTag('x',namespace=NS_MUC).setTagData('password',CONF[1])
