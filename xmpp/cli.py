a59992730edabf5f19d04b773e65cd397cfcef90
Andreas Motl <andreas.motl@panodata.org>
2021-12-28 02:32:56 +0100

    xmpp-message: Optionally read credentials from `~/.xsend` file

--- xmpp/cli.py
+++ xmpp/cli.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
+from collections import OrderedDict
 
 import xmpp
 import argparse
@@ -13,7 +15,9 @@ Synopsis::
 
 
 def send_message(jabberid, password, receiver, message, debug=False):
-
+    """
+    Connect to XMPP server and send message.
+    """
     if debug:
         debug = ['always']
 
@@ -27,14 +31,44 @@ def send_message(jabberid, password, receiver, message, debug=False):
     connection.send(xmpp.protocol.Message(to=receiver, body=message))
 
 
+def read_xsend():
+    """
+    Read credentials from `~/.xsend` file.
+    """
+    params = OrderedDict()
+    xsendfile = os.path.join(os.environ['HOME'], '.xsend')
+    with open(xsendfile, "r") as f:
+        for ln in f.readlines():
+            ln = ln.strip()
+            if not ln:
+                continue
+            if ln[0] not in ('#', ';'):
+                key, val = ln.split('=', 1)
+                params[key.lower()] = val
+    return params
+
+
 def simple_message():
+    """
+    Send an XMPP message from the command line.
+    """
     parser = argparse.ArgumentParser(description='Authenticate with XMPP server and send simple message')
-    parser.add_argument('--jabberid', required=True, help='Jabber Identifier (JID) aka. username')
-    parser.add_argument('--password', required=True, help='Password for Jabber Identifier (JID)')
+    parser.add_argument('--jabberid', required=False, help='Jabber Identifier (JID) aka. username')
+    parser.add_argument('--password', required=False, help='Password for Jabber Identifier (JID)')
     parser.add_argument('--receiver', required=True, help='Receiver address')
     parser.add_argument('--message', required=True, help='Message content')
     parser.add_argument('--debug', action='store_true', default=False, help='Enable debug messages')
 
     options = parser.parse_args()
+
+    # Optionally, read credentials from `~/.xsend` file.
+    if options.jabberid is None:
+        try:
+            params = read_xsend()
+        except Exception as ex:
+            sys.stderr.write("ERROR: Unable to read credentials from ~/.xsend file: %s\n" % ex)
+            sys.exit(1)
+        options.jabberid = params["jid"]
+        options.password = params["password"]
 
     send_message(options.jabberid, options.password, options.receiver, options.message, debug=options.debug)
