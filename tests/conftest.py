b32a1524a2b26510444d21102ef40a50bac6fc45
Andreas Motl <andreas.motl@panodata.org>
2026-01-06 23:37:50 +0100

    CI: Validate on Python 2.7

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ import pytz
 def run(capfd):
 
     def run_command(command, more_options=""):
-        command = f'{command} {more_options}'
+        command = "{command} {more_options}".format(command=command, more_options=more_options)
         subprocess.check_call(shlex.split(command))
         return capfd.readouterr()
 
@@ -18,7 +18,7 @@ def run(capfd):
 
 
 @pytest.fixture
-def timestamp_iso() -> str:
+def timestamp_iso():
     now = dt.datetime.now(tz=pytz.timezone("Europe/Berlin"))
     now = now - dt.timedelta(microseconds=now.microsecond)
     return now.isoformat()
