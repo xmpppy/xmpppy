f5afc2c0521c52827e56a4d80a2937f295187cd9
Andreas Motl <andreas.motl@panodata.org>
2026-01-24 23:48:36 +0100

    Tests: Validate compilation of example programs (Python 2)

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,6 +1,12 @@
 import logging
 import py_compile
-from pathlib import Path
+import sys
+from six import StringIO
+
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +18,26 @@ def test_compile():
     Validate if all provided example programs compile well.
     https://stackoverflow.com/a/4537442
     """
+
+    # Save stderr, because the test function redirects it.
+    real_stderr = getattr(sys, "stderr")
+
+    # List of examples written for Python 3.
+    python3_examples = ["muc.py"]
+
     for example in examples_path.glob("*.py"):
-        if not py_compile.compile(str(example)):
-            raise RuntimeError("Failed to compile %s" % (example,))
+
+        # On Python 2, skip examples written for Python 3.
+        if sys.version_info < (3, 0, 0) and example.name in python3_examples:
+            continue
+
+        # Compile program. If that emits anything to stderr, bail out.
+        buffer = StringIO()
+        setattr(sys, "stderr", buffer)
+        py_compile.compile(str(example))
+        output = buffer.getvalue().strip()
+        if output != "":
+            raise RuntimeError("Failed to compile %s: %s" % (example, output))
+
+    # Restore stderr.
+    setattr(sys, "stderr", real_stderr)
